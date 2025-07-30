85b42c5f59abea5296f7e69e9b615ea79176c16d
Nishimagizwe Ruben <rnishimagizwe@gmail.com>
2025-07-30 22:45:03 +0200

    update

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+// app/dashboard/page.jsx (Next.js 13+ app directory)
+
+import DashboardLayout from "../../components/dashboard/page";
+
+export default function DashboardPage() {
+  return (
+    <DashboardLayout>
+      <h1 className="text-2xl font-semibold mb-4">Bienvenue sur le Dashboard</h1>
+      {/* Ton contenu ici */}
+    </DashboardLayout>
+  );
+}
