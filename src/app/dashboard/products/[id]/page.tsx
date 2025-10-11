93c17e20e90ac29b44a70a7d7c24f3a1326911c4
kanamba123 <115944232+kanamba123@users.noreply.github.com>
2025-10-12 01:19:12 +0200

    update

--- src/app/dashboard/products/[id]/page.tsx
+++ src/app/dashboard/products/[id]/page.tsx
@@ -187,7 +187,7 @@ export default function EditVariantForm() {
   }
 
   return (
-    <div className="max-w-7xl mx-auto p-4 sm:p-6 mt-10 bg-[var(--color-bg-primary)] text-[var(--color-text-primary)] shadow-md dark:shadow-lg rounded-lg ">
+    <div className="max-w-7xl mx-auto p-4 sm:p-6 mt-2 bg-[var(--color-bg-primary)] text-[var(--color-text-primary)] shadow-md dark:shadow-lg rounded-lg ">
       {submitted && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-xl text-center">
@@ -199,7 +199,7 @@ export default function EditVariantForm() {
         </div>
       )}
 
-      <h1 className="text-2xl font-bold mb-6 text-center">✏️ Modifier une Variante de Produit</h1>
+      <h1 className="text-2xl font-bold mb-6 text-center">Modifier une Variante de Produit</h1>
 
       <form onSubmit={handleSubmit((data) => {
         setShowConfirmSave(true)
