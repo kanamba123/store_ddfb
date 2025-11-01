7992f1a60267b6484767a85c90dbbb11a62de06a
kanamba123 <115944232+kanamba123@users.noreply.github.com>
2025-11-01 19:01:12 +0100

    update

--- src/services/deleteImageFromFirebase.js
+++ src/services/deleteImageFromFirebase.js
@@ -1,11 +1,11 @@
 // src/services/deleteImageFromFirebase.js
-import { storage } from "../firebaseConfig"; 
+import { storage } from "../firebaseConfig";
 
 
 import { ref, deleteObject } from "firebase/storage"; // Importer les méthodes nécessaires
 // src/services/deleteImageFromFirebase.js
 
-export const deleteImageFromFirebase = async (imageUrl, onDeleteSuccess) => {
+export const deleteImageFromFirebase = async (imageUrl, onDeleteSuccess = () => { }) => {
   if (!imageUrl) {
     // Si imageUrl est null ou undefined, ne rien faire
     return;
@@ -16,28 +16,28 @@ export const deleteImageFromFirebase = async (imageUrl, onDeleteSuccess) => {
     const path = imageUrl;
 
     // Créer une référence au fichier dans Firebase Storage
-    
-     let fileRef
 
-      if (Array.isArray(path)) {
-        // Si imageUrl est un tableau, tenter de supprimer chaque URL
-        for (const url of path) {
+    let fileRef
 
-         fileRef = ref(storage, url);
-          await deleteObject(fileRef);
-          onDeleteSuccess();
-        }
-      } else if (typeof path === "string") {
-        fileRef = ref(storage, path);
-        // Supprimer l'image du stockage Firebase
+    if (Array.isArray(path)) {
+      // Si imageUrl est un tableau, tenter de supprimer chaque URL
+      for (const url of path) {
+
+        fileRef = ref(storage, url);
         await deleteObject(fileRef);
         onDeleteSuccess();
-      } else {
-        console.error("Type de imageUrl non supporté :", typeof imageUrl);
       }
+    } else if (typeof path === "string") {
+      fileRef = ref(storage, path);
+      // Supprimer l'image du stockage Firebase
+      await deleteObject(fileRef);
+      onDeleteSuccess();
+    } else {
+      console.error("Type de imageUrl non supporté :", typeof imageUrl);
+    }
 
-    
-    
+
+
   } catch (error) {
     console.error("Erreur lors de la suppression de l'image :", error);
   }
